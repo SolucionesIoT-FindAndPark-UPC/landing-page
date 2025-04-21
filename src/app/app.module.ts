e13becde4c3509bcd729c358f40f9186623ac099
Eric Cuevas <u202112766@upc.edu.pe>
2025-04-20 21:41:18 -0500

    fix: footer module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FooterComponent } from './Footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
