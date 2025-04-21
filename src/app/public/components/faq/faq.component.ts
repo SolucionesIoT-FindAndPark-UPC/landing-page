e41a8847894590a5a68c2d5772ddf9824a821daa
Lucero <u202111465@upc.edu.pe>
2025-04-20 21:52:11 -0500

    feat: Add questions and answers

--- src/app/public/components/faq/faq.component.ts
+++ src/app/public/components/faq/faq.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
+  imports: [CommonModule],
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.css'
+  styleUrls: ['./faq.component.css']
 })
-export class FaqComponent {
+export class FaqComponent implements OnInit {
+  faqs = [
+    {
+      question: 'How do I download the app?',
+      answer: 'You can download the app from the App Store or Google Play Store.',
+      isOpen: false
+    },
+    {
+      question: 'What payment methods does ParkUp accept?',
+      answer: 'ParkUp accepts credit and debit cards.',
+      isOpen: false
+    },
+    {
+      question: 'Do I need to print a ticket?',
+      answer: 'No, you can simply show your digital ticket in the app.',
+      isOpen: false
+    },
+    {
+      question: 'Is ParkUp available in all parking lots?',
+      answer: 'ParkUp is available in most major parking lots, but please check our website for availability.',
+      isOpen: false
+    },
+    {
+      question: 'Is it safe to pay through the app?',
+      answer: 'Yes, we use secure encryption and trusted payment processors to protect your information.',
+      isOpen: false
+    }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  toggleAnswer(faq: any): void {
+    faq.isOpen = !faq.isOpen; // Toggle the open/close state
+  }
 }
