3e00d478c10854d645484174d75e720a2af5b5db
Luis Herrera <u202218227@upc.edu.pe>
2025-05-16 00:50:27 -0500

    feat: made header and hero section responsive.

--- src/app/public/components/toolbar/toolbar.component.ts
+++ src/app/public/components/toolbar/toolbar.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faBars, faHamburger} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
+  standalone: true,
+  imports: [
+    FaIconComponent
+  ],
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+  protected readonly faBars = faBars;
+  sideMenuOpen = false;
 
+  openSideMenu() {
+    this.sideMenuOpen = true;
+  }
+  closeSideMenu() {
+    this.sideMenuOpen = false;
+  }
 }
